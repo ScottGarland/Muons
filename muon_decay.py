a72ef583ff084d2409fb8aa481bbecc1c765c5f9
ScottGarland <scott.a.garland@protonmail.com>
2023-11-18 15:44:16 -0500

    starting matplotlib of old data

---
+++ muon_decay.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import scipy
+import matplotlib.pyplot as plt
+
+
+t_var = []
+y_var = []
+
+# N0 initial quantity, tau is lifetime, B is background constant
+def exp_decay(t, N0, tau, B):
+    return N0 * np.exp(-t/tau) + B
+
+def plot():
+    return None
+
+raw_data = []
+with open("data/microsecond_count_1500.dat") as raw:
+    for line in raw:
+        raw_data.append(line)
+
+data = []
+for element in raw_data:
+    data.append(element.split('\t'))
+
+for element in data:
+    t_var.append(float(element[0]))
+    y_split = element[1].split('\n')
+    y_var.append(int(y_split[0]))
+
+popt, pcov = scipy.optimize.curve_fit(exp_decay, t_var, y_var)
+N0, tau, B = popt
+
+plt.scatter(t_var, y_var)
+plt.show()
+
+if __name__ == '__main__':
+    main()
